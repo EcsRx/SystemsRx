0a9953e5b1c64d51103af4cd722410d4295a5804
LP <grofit1@yahoo.com>
2021-06-28 21:46:24 +0100

    Updated some tests from EcsRx

---
+++ src/SystemsRx.Tests/Systems/Handlers/LowerPriorityHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using SystemsRx.Attributes;
+using SystemsRx.Executor.Handlers;
+using SystemsRx.Systems;
+using SystemsRx.Types;
+
+namespace SystemsRx.Tests.Systems.Handlers
+{
+    [Priority(PriorityTypes.Low)]
+    public class LowerPriorityHandler : IConventionalSystemHandler
+    {
+        private Action _doSomethingOnSetup;
+        private Action _doSomethingOnDestroy;
+
+        public LowerPriorityHandler(Action doSomethingOnSetup = null, Action doSomethingOnDestroy = null)
+        {
+            _doSomethingOnSetup = doSomethingOnSetup;
+            _doSomethingOnDestroy = doSomethingOnDestroy;
+        }
+        
+        public void Dispose() {}
+        public bool CanHandleSystem(ISystem system) => true;
+
+        public void SetupSystem(ISystem system)
+        { _doSomethingOnSetup?.Invoke(); }
+
+        public void DestroySystem(ISystem system)
+        { _doSomethingOnDestroy?.Invoke(); }
+    }
+}
