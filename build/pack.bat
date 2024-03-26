63d22c0089002db3477f516c9143d9be1d2ed82b
LP <grofit1@yahoo.com>
2024-03-26 11:31:13 +0000

    Adding build pack for new packages

--- build/pack.bat
+++ build/pack.bat
@@ -5,4 +5,7 @@ dotnet pack ../src/SystemsRx.Infrastructure -c Release -o ../../_dist /p:version
 dotnet pack ../src/SystemsRx.Plugins.Computeds -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/SystemsRx.Infrastructure -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/SystemsRx.Infrastructure.Ninject -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/SystemsRx.Infrastructure.Autofac -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/SystemsRx.Infrastructure.MicrosoftDependencyInjection -c Release -o ../../_dist /p:version=%version%
+dotnet pack ../src/SystemsRx.Infrastructure.DryIoc -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/SystemsRx.ReactiveData -c Release -o ../../_dist /p:version=%version%
