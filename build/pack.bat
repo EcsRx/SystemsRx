d04e5e91ca4f2b71408ea4391f46cc7acc0c2b02
LP <grofit1@yahoo.com>
2024-03-26 13:30:54 +0000

    Updating build and api keys

--- build/pack.bat
+++ build/pack.bat
@@ -1,4 +1,4 @@
-set version=5.0.0
+set version=7.0.0
 dotnet pack ../src/SystemsRx.MicroRx -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/SystemsRx -c Release -o ../../_dist /p:version=%version%
 dotnet pack ../src/SystemsRx.Infrastructure -c Release -o ../../_dist /p:version=%version%
