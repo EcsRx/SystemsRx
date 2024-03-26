07af50162c7a68ab0eb1376f6956057f5ffad43b
LP <grofit1@yahoo.com>
2024-03-26 13:58:39 +0000

    Updating .net standard and C# version as well as bump appveyor image

--- src/SystemsRx.Infrastructure/Dependencies/IDependencyResolver.cs
+++ src/SystemsRx.Infrastructure/Dependencies/IDependencyResolver.cs
@@ -1,31 +1,32 @@
 using System;
 using System.Collections;
 
-namespace SystemsRx.Infrastructure.Dependencies;
-
-/// <summary>
-/// Provides a cross platform way to resolve any dependencies that have been registered
-/// </summary>
-public interface IDependencyResolver : IDisposable
+namespace SystemsRx.Infrastructure.Dependencies
 {
     /// <summary>
-    /// This exposes the underlying DI resolver, but any calls to this directly
-    /// will not be cross platform, so be weary if you need it or not.
+    /// Provides a cross platform way to resolve any dependencies that have been registered
     /// </summary>
-    object NativeResolver { get; }
-        
-    /// <summary>
-    /// Gets an enumerable of a given type from the underlying DI resolver
-    /// </summary>
-    /// <param name="type">Type to resolve</param>
-    /// <returns>All matching instances of that type within the underlying container</returns>
-    IEnumerable ResolveAll(Type type);
+    public interface IDependencyResolver : IDisposable
+    {
+        /// <summary>
+        /// This exposes the underlying DI resolver, but any calls to this directly
+        /// will not be cross platform, so be weary if you need it or not.
+        /// </summary>
+        object NativeResolver { get; }
 
-    /// <summary>
-    /// Gets an instance of a given type from the underlying DI container
-    /// </summary>
-    /// <param name="type">Type of the object you want</param>
-    /// <param name="name">Optional name of the binding</param>
-    /// <returns>An instance of the given type</returns>
-    object Resolve(Type type, string name = null);
+        /// <summary>
+        /// Gets an enumerable of a given type from the underlying DI resolver
+        /// </summary>
+        /// <param name="type">Type to resolve</param>
+        /// <returns>All matching instances of that type within the underlying container</returns>
+        IEnumerable ResolveAll(Type type);
+
+        /// <summary>
+        /// Gets an instance of a given type from the underlying DI container
+        /// </summary>
+        /// <param name="type">Type of the object you want</param>
+        /// <param name="name">Optional name of the binding</param>
+        /// <returns>An instance of the given type</returns>
+        object Resolve(Type type, string name = null);
+    }
 }
--- src/SystemsRx.Infrastructure/Extensions/IDependencyResolverExtensions.cs
+++ src/SystemsRx.Infrastructure/Extensions/IDependencyResolverExtensions.cs
@@ -2,25 +2,30 @@ using System.Collections.Generic;
 using System.Linq;
 using SystemsRx.Infrastructure.Dependencies;
 
-namespace SystemsRx.Infrastructure.Extensions;
-
-public static class IDependencyResolverExtensions
+namespace SystemsRx.Infrastructure.Extensions
 {
-    /// <summary>
-    /// Gets an instance of a given type from the underlying DI container
-    /// </summary>
-    /// <param name="resolver">The container to resolve from</param>
-    /// <param name="name">Optional name of the binding</param>
-    /// <typeparam name="T">Type of the object you want</typeparam>
-    /// <returns>An instance of the given type</returns>
-    public static T Resolve<T>(this IDependencyResolver resolver, string name = null)
-    { return (T)resolver.Resolve(typeof(T), name); }
-        
-    /// <summary>
-    /// Gets an enumerable of a given type from the underlying DI container
-    /// </summary>
-    /// <typeparam name="T">Type to resolve</typeparam>
-    /// <returns>All matching instances of that type within the underlying container</returns>
-    public static IEnumerable<T> ResolveAll<T>(this IDependencyResolver resolver)
-    { return resolver.ResolveAll(typeof(T)).Cast<T>(); }
+    public static class IDependencyResolverExtensions
+    {
+        /// <summary>
+        /// Gets an instance of a given type from the underlying DI container
+        /// </summary>
+        /// <param name="resolver">The container to resolve from</param>
+        /// <param name="name">Optional name of the binding</param>
+        /// <typeparam name="T">Type of the object you want</typeparam>
+        /// <returns>An instance of the given type</returns>
+        public static T Resolve<T>(this IDependencyResolver resolver, string name = null)
+        {
+            return (T)resolver.Resolve(typeof(T), name);
+        }
+
+        /// <summary>
+        /// Gets an enumerable of a given type from the underlying DI container
+        /// </summary>
+        /// <typeparam name="T">Type to resolve</typeparam>
+        /// <returns>All matching instances of that type within the underlying container</returns>
+        public static IEnumerable<T> ResolveAll<T>(this IDependencyResolver resolver)
+        {
+            return resolver.ResolveAll(typeof(T)).Cast<T>();
+        }
+    }
 }
