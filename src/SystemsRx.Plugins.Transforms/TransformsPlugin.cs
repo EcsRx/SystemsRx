21ff8be2066e102691000719f863b8397a710e05
LP <grofit@users.noreply.github.com>
2024-03-30 13:58:09 +0000

    Adds a plugin that provides cross platform transform objects for 3d/2d (#4)

---
+++ src/SystemsRx.Plugins.Transforms/TransformsPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemsRx.Infrastructure.Dependencies;
+using SystemsRx.Infrastructure.Plugins;
+using SystemsRx.Systems;
+
+namespace SystemsRx.Plugins.Transforms
+{
+    public class TransformsPlugin : ISystemsRxPlugin
+    {
+        public string Name => "Transforms Plugin";
+        public Version Version { get; } = new Version("1.0.0");
+
+        public void SetupDependencies(IDependencyRegistry registry)
+        {
+            // Nothing needs registering
+        }
+        
+        public IEnumerable<ISystem> GetSystemsForRegistration(IDependencyResolver resolver) => Array.Empty<ISystem>();
+    }
+}
