884a0ea10b0391d25c4290b7624a1ca808498bb5
LP <grofit1@yahoo.com>
2024-04-30 15:26:16 +0100

    Made recommended Codacity changes

--- src/SystemsRx.Plugins.Transforms/Extensions/Transform2DExtensions.cs
+++ src/SystemsRx.Plugins.Transforms/Extensions/Transform2DExtensions.cs
@@ -6,9 +6,9 @@ namespace SystemsRx.Plugins.Transforms.Extensions
 {
     public static class Transform2DExtensions
     {
-        public const float RadiansToDegrees = 180.0f / MathF.PI;
-        public const float DegreesToRadians = MathF.PI / 180.0f;
-        public const float Radian90Degrees = 90 * DegreesToRadians;
+        public static readonly float RadiansToDegrees = 180.0f / MathF.PI;
+        public static readonly float DegreesToRadians = MathF.PI / 180.0f;
+        public static readonly float Radian90Degrees = 90 * DegreesToRadians;
         
         /// <summary>
         /// Returns the transforms rotation but converted to degrees
